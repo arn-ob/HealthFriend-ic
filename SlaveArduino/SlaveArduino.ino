adc21b202638e91e41752bb9c337993f7d7a0e33
Arnob Almazee <arnobxtreme@gmail.com>
2019-01-22 01:34:07 +0600

    upload

---
+++ SlaveArduino/SlaveArduino.ino
@@ -0,0 +1,23 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(10, 11); // RX, TX
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+
+  Serial.println("Goodnight moon!");
+
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(9600);
+  mySerial.println("Hello, world?");
+}
+
+void loop() { // run over and over
+   mySerial.println("Hello, world?");
+  delay(2000);
+}
